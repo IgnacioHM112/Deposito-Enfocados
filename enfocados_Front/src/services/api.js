7f2ffe37e324c868f23fa6f0fe4a17d131c68b7e
Ignacio Mello <ignamello2002@gmail.com>
2026-09-21 20:05:59 -0300

    Trabajando en el front

--- enfocados_Front/src/services/api.js
+++ enfocados_Front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://straining-fiscally-regulator.ngrok-free.dev',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3002/api',
   headers: {
     'Content-Type': 'application/json',
   },
